70aa2c11884c1fa35ef000364ba3369f3c005186
Rashid Shafeev <rashidshafeev@gmail.com>
2024-01-27 19:34:12 +0300

    Автоматический литер; Валидация формы + очищение формы; Поменял способ раскрашивания аватарок

--- src/components/AddUserForm.tsx
+++ src/components/AddUserForm.tsx
@@ -7,9 +7,11 @@ import './AddUserForm.css'
 
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser } from '../store/users';
-import { Card, Typography, Grid, Stack, CardContent, CardHeader, Divider } from '@mui/material';
+import { Card, Typography, Grid, Stack, CardContent, CardHeader, Divider,Alert } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import LoadingButton from '@mui/lab/LoadingButton';
+
+import * as Yup from 'yup';
 
 interface Values {
   name: string;
@@ -17,10 +19,22 @@ interface Values {
   email: string;
 }
 
+const AddUserSchema = Yup.object().shape({
+  name: Yup.string()
+    .max(50, 'Слишком длинное имя')
+    .required('Введите имя'),
+  username: Yup.string()
+    .min(2, 'Слишком короткий юзернейм')
+    .max(50, 'Слишком длинный юзернейм')
+    .required('Введите юзернейм'),
+  email: Yup.string().email('Неверный формат email').required('Введите email'),
+});
+
+
 function AddUserForm() {
 
   const dispatch = useDispatch()
-  const addIsLoading : boolean = useSelector((state: RootState) => state.users.addIsLoading);
+  const addIsLoading: boolean = useSelector((state: RootState) => state.users.addIsLoading);
 
   return (
     <>
@@ -31,58 +45,63 @@ function AddUserForm() {
           username: '',
           email: '',
         }}
+        validationSchema={AddUserSchema}
         onSubmit={(
           values: Values,
-          { setSubmitting }: FormikHelpers<Values>
+          { setSubmitting, resetForm }: FormikHelpers<Values>
         ) => {
           dispatch(addUser(values))
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
+          resetForm({
+            values: {
+              name: '',
+              username: '',
+              email: '',
+            },
+          });
         }}
-      > 
+      >
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
             <Card>
               <CardHeader
-              title="Добавить пользователя"
+                title="Добавить пользователя"
               >
 
               </CardHeader>
               <Divider></Divider>
               <CardContent>
-              {/* <Form className='add-user-form'> */}
-              <Form>
-                <Stack
-                direction="column">
-                <Typography variant="h5" gutterBottom>
-                  
-                </Typography>
-                <label htmlFor="name">Имя пользователя</label>
-                <Field className="form-input" id="name" name="name" placeholder="Ваше имя" />
+                <Form>
+                  <Stack
+                    direction="column">
+                    <Typography variant="h5" gutterBottom>
 
-                <label htmlFor="username">Юзернейм</label>
-                <Field className="form-input" id="username" name="username" placeholder="Имя пользователя" />
+                    </Typography>
+                    <label htmlFor="name">Имя пользователя</label>
+                    <Field className="form-input" id="name" name="name" placeholder="Ваше имя" />
+                    <ErrorMessage name="name" render={msg => <Alert severity="warning">{msg}</Alert>}/>
 
-                <label htmlFor="email">Email</label>
-                <Field
-                  className="form-input"
-                  id="email"
-                  name="email"
-                  placeholder="john@acme.com"
-                  type="email"
-                />
+                    <label htmlFor="username">Юзернейм</label>
+                    <Field className="form-input" id="username" name="username" placeholder="Имя пользователя" />
+                    <ErrorMessage name="username" render={msg => <Alert severity="warning">{msg}</Alert>}/>
+                    <label htmlFor="email">Email</label>
+                    <Field
+                      className="form-input"
+                      id="email"
+                      name="email"
+                      placeholder="john@acme.com"
+                      type="email"
+                    />
+                    <ErrorMessage name="email" render={msg => <Alert severity="warning">{msg}</Alert>}/>
 
                     {!addIsLoading && <Button size="large" variant="contained" color="success" type="submit" sx={{
                       width: "60%", margin: "20px 0 0 0", alignSelf: "flex-end"
                     }}
-                    startIcon={<AddIcon/>}>ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ</Button>}   
-    {addIsLoading && <LoadingButton loading size="large"  variant="contained" sx={{
+                      startIcon={<AddIcon />}>ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ</Button>}
+                    {addIsLoading && <LoadingButton loading size="large" variant="contained" sx={{
                       width: "60%", margin: "20px 0 0 0", alignSelf: "flex-end"
-                    }}>ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ</LoadingButton>}  
-                </Stack>
-              </Form>
+                    }}>ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ</LoadingButton>}
+                  </Stack>
+                </Form>
               </CardContent>
             </Card>
           </Grid>
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,68 +2,89 @@ import React from 'react'
 import { BlogUser, deleteUser } from '../store/users';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Avatar, CardHeader, Divider, Grid, colors,CardContent,Card, Stack, Typography } from '@mui/material';
+import { Avatar, CardHeader, Divider, Grid, colors, CardContent, Card, Stack, Typography } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkIcon from '@mui/icons-material/Link';
 
 interface UserCardProps {
     user: BlogUser,
-  }
-function UserCard(props : UserCardProps) {
+}
 
+
+function UserCard(props: UserCardProps) {
+
+    function stringToColor(string: string) {
+        let hash = 0;
+        let i;
+
+        /* eslint-disable no-bitwise */
+        for (i = 0; i < string.length; i += 1) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        let color = '#';
+
+        for (i = 0; i < 3; i += 1) {
+            const value = (hash >> (i * 8)) & 0xff;
+            color += `00${value.toString(16)}`.slice(-2);
+        }
+        /* eslint-enable no-bitwise */
+
+        return color;
+    }
     const colorsArray = [
         colors.red[500], colors.green[500], colors.blue[500], colors.cyan[500]
     ]
-    
-const dispatch = useDispatch()
 
-const deleteUserHandler = () => {
-    dispatch(deleteUser(props.user.id))
-}
+    const dispatch = useDispatch()
 
-  return (
-    <Grid item xs={6} sm={3}>
+    const deleteUserHandler = () => {
+        dispatch(deleteUser(props.user.id))
+    }
 
-    
-    <Card>
-        <CardHeader
-        onClick={deleteUserHandler}
-        avatar={
-            <Avatar variant="rounded" sx={{ bgcolor: colorsArray[Math.floor(Math.random()*4)]  }} aria-label="recipe">
-              {props.user.name.slice(0,1)}
-            </Avatar>
-          }
-          title={props.user.name}
-        >
-        
-        </CardHeader>
-        
-        <Divider />
-        <CardContent>
-        <Stack
-        direction="row">
-            <LinkIcon/>
-            <Typography
-            color="text.secondary"
-            margin="0 0 0 10px">
-            {props.user.username}
-            </Typography>
-        </Stack>
-        <Stack
-        direction="row">
-            <EmailIcon />
-            <Typography
-            color="text.secondary"
-            margin="0 0 0 10px">
-            {props.user.email}
-            </Typography>
-        </Stack>
-        
-        </CardContent>
-        
-    </Card>
-    </Grid>
-  )
+    return (
+        <Grid item xs={6} sm={3}>
+
+
+            <Card>
+                <CardHeader
+                    onClick={deleteUserHandler}
+                    avatar={
+                        <Avatar variant="rounded" sx={{ bgcolor: stringToColor(props.user.name) }} aria-label="recipe">
+                            {props.user.name.slice(0, 1)}
+                        </Avatar>
+                    }
+                    title={props.user.name}
+                >
+
+                </CardHeader>
+
+                <Divider />
+                <CardContent>
+                    <Stack
+                        direction="row">
+                        <LinkIcon />
+                        <Typography
+                            color="text.secondary"
+                            margin="0 0 0 10px">
+                            {props.user.username}
+                        </Typography>
+                    </Stack>
+                    <Stack
+                        direction="row">
+                        <EmailIcon />
+                        <Typography
+                            color="text.secondary"
+                            margin="0 0 0 10px">
+                            {props.user.email}
+                        </Typography>
+                    </Stack>
+
+                </CardContent>
+
+            </Card>
+        </Grid>
+    )
 }
 
 export default UserCard
